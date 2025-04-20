39a2ae28cd1451535635f913c985a82ca272c53c
Martin <ms@msauter.at>
2025-04-20 21:55:50 +0200

    Update tgdb_update.sh

--- tgdb_update.sh
+++ tgdb_update.sh
@@ -2,13 +2,13 @@
 
 cd /var/www/html/include
 
-sudo rm tgdb.php
+rm tgdb.php
 
-sudo wget https://raw.githubusercontent.com/OE9SAU/SVXLink_Git/refs/heads/main/tgdb.txt
+wget https://raw.githubusercontent.com/OE9SAU/SVXLink_Git/refs/heads/main/tgdb.txt
 
-sudo cp /var/www/html/include/tgdb.txt /var/www/html/include/tgdb.php
+cp /var/www/html/include/tgdb.txt /var/www/html/include/tgdb.php
 
-sudo chown svxlink:svxlink /var/www/html/include/tgdb.txt /var/www/html/include/tgdb.php
-sudo chmod 755 /var/www/html/include/tgdb.txt /var/www/html/include/tgdb.php 
+chown svxlink:svxlink /var/www/html/include/tgdb.txt /var/www/html/include/tgdb.php
+chmod 755 /var/www/html/include/tgdb.txt /var/www/html/include/tgdb.php 
 
-sudo rm tgdb.txt
+rm tgdb.txt
